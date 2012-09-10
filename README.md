a4408ef26fd230446648b0a176c08a0ec598a727
Xavier Noria <fxn@hashref.com>
2012-09-10 08:56:40 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,91 @@
+# Terminal Keynote
+
+![Terminal Keynote Cover](https://raw.github.com/fxn/tkn/master/screenshots/terminal-keynote-cover.png)
+
+## Introduction
+
+Terminal Keynote is a quick and dirty script I wrote for presenting my talks at [BaRuCo 2012](http://baruco.org) and [RailsClub 2012](http://railsclub.ru).
+
+This is a total hack. It is procedural, uses a global variable, it has not been parametrized or generalized in anyway. It was tailor-made for what I exactly wanted but some people in the audience asked for the script. Even if it is quick and dirty I am very happy to share it so I have commented the source code and there you go!
+
+## Markup
+
+Fuck markup, this is text going to a terminal. If you want a list type "*"s. If you want bold face or colors use ANSI escape sequences.
+
+Slides are written in Ruby. See the examples folder.
+
+## Syntax Highlighting
+
+Terminal Keynote is text-based, but with style! Syntax highlighting is done on the fly with @tmm1's [pygments.rb](https://github.com/tmm1/pygments.rb). The script uses the "terminal256" formatter and "bw" style, the lexer is also hard-coded to "ruby". Since this was tailor-made it has not been factored out.
+
+## Master Slides
+
+There are four types of slides:
+
+### :code
+
+A slide with source code. Syntax highlighted on the fly. If you want to put a title or file name or something use source code comments and imagination.
+
+![Terminal Keynote Code](https://raw.github.com/fxn/tkn/master/screenshots/terminal-keynote-code.png)
+
+### :center
+
+A slide whose text is centered line by line.
+
+![Terminal Keynote Center](https://raw.github.com/fxn/tkn/master/screenshots/terminal-keynote-center.png)
+
+### :block
+
+A slide with text content whose formatting is preserved, but that is centered as a whole in the screen. Do that with CSS, ha!
+
+I find centering content in the screen as a block to be more aesthetically pleasant that flushing against the left margin. There is not way to flush against a margin.
+
+![Terminal Keynote Block](https://raw.github.com/fxn/tkn/master/screenshots/terminal-keynote-block.png)
+
+### Sections
+
+Sections have a title and draw kind of a fleuron. This is also hard-coded because it is what I wanted.
+
+Sections allow you to group slides in your Ruby slide deck, and since they yield to a block you can collapse/fold the ones you are not working in for focus.
+
+The nested structure is not modelled internally. The script only sees a flat linear sequence of slides.
+
+![Terminal Keynote Section](https://raw.github.com/fxn/tkn/master/screenshots/terminal-keynote-section.png)
+
+## Visual Effects
+
+There is a hard-coded visual effect: Once the exact characters of a given slide are computed, we print char by char with a couple milliseconds in between. That gives the illusion of an old-school running cursor effect. Configure block blinking cursor for maximum awesomeness.
+
+## Installation
+
+By now this is not going to be a gem, please clone the repo and hack your talk. In its current state it is just too tailor-made for anything but personal forks. Please keep the script together with the slides, that way you guarantee one year later the presentation will still run.
+
+If Terminal Keynote evolves it is going to do so in backwards incompatible ways for sure. So, let's wait. If the thing ever converges to something that can be packaged then I'll do it.
+
+## Keyboard Controls and Remotes
+
+* To go forward press any of " ", "n", "k", "l", PageDown (but see below).
+
+* To go backwards press any of "b", "p", "h", "j", PageUp (but see below).
+
+* Beginning of the talk: "^"
+
+* End of the talk: "$"
+
+My Logitech remote emits PageDown and PageUp. You get those as ANSI escape sequences "\e[5~" and "\e[6~" respectively. The script understands them, but you need to [configure them in Terminal.app](http://fplanque.com/dev/mac/mac-osx-terminal-page-up-down-home-end-of-line) and also tell it to pass them down to the shell selecting "send string to the shell" in the "Action" selector.
+
+## Font and Terminal Configuration
+
+I used Menlo, 32 points. That gives 18x52 in a screen resolution of 1024x768.
+
+For your setup: Find out the resolution of the projector of your conference (ask the organization in advance). Set the screen to that resolution, choose font size and maximize window. When you like how it looks, then run `stty size` and write down rows and cols.
+
+Then, define in your terminal application a profile for the theme you like, and initial dimensions to those rows and cols. That way the terminal will launch with those dimensions no matter the screen resolution and you can hack your talk in your day to day with the native resolution, knowing how is going to look in proportion.
+
+## Editor Snippets
+
+A snippet for your editor is basic to write slides quickly. The extras folder has a snippet for Sublime Text 2.
+
+## Cathode
+
+[Cathode](http://www.secretgeometry.com/apps/cathode/) is perfect for this thing. But because of how it draws the text it doesn't do bold faces and may not be able to render some colors or Unicode characters. YMMV.
---
+++ examples/constant_autoloading_in_ruby_on_rails.rb
@@ -0,0 +1,425 @@
+# encoding: utf-8
+
+slide <<-EOS, :center
+  \e[1mConstant Autoloading in Ruby on Rails\e[0m
+
+
+  Xavier Noria
+  @fxn
+
+  BaRuCo 2012
+EOS
+
+slide <<-EOS, :code
+  require 'application_controller'
+  require 'post'
+
+  class PostsController < ApplicationController
+    def index
+      @posts = Post.all
+    end
+  end
+EOS
+
+slide <<-EOS, :code
+  class PostsController < ApplicationController
+    def index
+      @posts = Post.all
+    end
+  end
+EOS
+
+section "Constants Refresher" do
+  slide <<-EOS, :code
+    X = 1
+  EOS
+
+  slide <<-EOS, :code
+    class C
+    end
+  EOS
+
+  slide <<-EOS, :code
+    # ordinary class definition
+    class C < D
+      include M
+    end
+
+    # equivalent modulus details
+    C = Class.new(D) do
+      include M
+    end
+    
+    # class name comes from the constant
+    C.name # => "C"
+  EOS
+
+  slide <<-EOS, :code
+    ArgumentError      FalseClass
+    Array              Fiber
+    BasicObject        File
+    Bignum             FileTest
+    Binding            Fixnum
+    Class              Float
+    Comparable         GC
+    Complex            Gem
+    Config             Hash
+    Dir                IO
+    Encoding           IOError
+    EncodingError      IndexError
+    Enumerable         Integer
+    Enumerator         Interrupt
+    Errno              Kernel
+    Exception          ...
+  EOS
+
+  slide <<-EOS, :block
+    Constants are stored in modules
+  EOS
+
+  slide <<-EOS, :code
+    # rubinius/kernel/common/module.rb
+    
+    class Module
+      attr_reader :constant_table
+      attr_writer :method_table
+      ...
+    end
+  EOS
+
+  slide <<-EOS, :code
+    module M
+      X = 1
+    end
+  EOS
+
+  slide <<-EOS, :code
+    # ordinary class definition in namespace
+    module XML
+      class SAXParser
+      end
+    end
+    
+    # equivalent modulus details to
+    module XML
+      SAXParser = Class.new
+    end
+  EOS
+
+  slide <<-EOS, :center
+    Constants API
+  EOS
+
+  slide <<-EOS, :center
+    Constant Name Resolution (1)
+  EOS
+
+  slide <<-EOS, :code
+    module M
+      X = 1
+    end
+  EOS
+
+  slide <<-EOS, :code
+    module Admin
+      class UsersController < ApplicationController
+      end
+    end
+  EOS
+
+  slide <<-EOS, :center
+    Constant Name Resolution (2)
+  EOS
+
+  slide <<-EOS, :code
+    M::X
+  EOS
+
+  slide <<-EOS, :center
+    Constant Name Resolution (3)
+  EOS
+
+  slide <<-EOS, :code
+    module M
+      module N
+        class C < D
+          X
+        end
+      end
+    end
+  EOS
+end
+
+section "Constant Autoloading" do
+  slide <<-EOS, :code
+    module Admin
+      class UsersController < ApplicationController
+        def index
+          @users = User.all
+        end
+      end
+    end
+  EOS
+
+  slide <<-EOS, :code
+    # active_support/dependencies.rb
+
+    def self.const_missing(const_name)
+      name       # => "Admin::UsersController"
+      const_name # => :User
+    end
+  EOS
+
+  slide <<-EOS, :code
+    config.autoload_paths
+  EOS
+
+  slide <<-EOS, :block
+    admin/users_controller/user.rb
+    admin/users_controller/user
+
+    # trade-offs
+
+    admin/user.rb
+    admin/user
+
+    # trade-offs
+
+    user.rb # FOUND
+  EOS
+
+  slide <<-EOS, :code
+    class Contact < ActiveRecord::Base
+      after_commit :register_event
+    end
+
+    def register_event
+      Worker::EventRegister.perform_async(...)
+    end
+  EOS
+
+  slide <<-EOS, :block
+    worker.rb
+    worker # FOUND
+  EOS
+
+  slide <<-EOS, :code
+    Object.const_set("Worker", Module.new)
+  EOS
+
+  slide <<-EOS, :block
+    We keep track of:
+
+      * Fully qualified names of autoloaded constants
+
+      * Their corresponding file names
+  EOS
+
+  slide <<-EOS, :center
+    Kernel#load and Kernel#require are wrapped
+  EOS
+
+  # slide <<-EOS, :center
+  #   require_dependency "sti_grandchildren"
+  # EOS
+
+  slide <<-EOS, :block
+    Problem: Which Is The Nesting?
+  EOS
+
+  slide <<-EOS, :code
+    # active_support/dependencies.rb
+
+    def self.const_missing(const_name)
+      name       # => "Admin::UsersController"
+      const_name # => :User
+    end
+  EOS
+
+  slide <<-EOS, :code
+    module M
+      module N
+        # nesting: [M::N, M]
+      end
+    end
+
+    module M::N
+      # nesting: [M::N]
+    end
+
+    module A::B
+      module M::N
+        # nesting: [M::N, A::B]
+      end
+    end
+  EOS
+
+  slide <<-EOS1, :code
+    module M
+      Module.new.module_eval <<-EOS
+        # nesting: [#<Module:0x007fa4a284f708>, M]
+      EOS
+    end
+  EOS1
+
+  slide <<-EOS, :center
+    Trade-off for Named Modules:
+  EOS
+
+  slide <<-EOS, :center
+    The name reflects the nesting
+  EOS
+
+  slide <<-EOS, :code
+    module M
+      module N
+        # nesting: [M::N, M]
+      end
+    end
+  EOS
+
+  slide <<-EOS, :center
+    Trade-off for Anonymous Modules
+  EOS
+
+  slide <<-EOS, :code
+    # active_support/dependencies.rb
+
+    def const_missing(const_name)
+      from_mod = anonymous? ? ::Object : self
+      ...
+    end
+  EOS
+
+  slide <<-EOS, :block
+    Problem: Which Is The Resolution Algorithm?
+  EOS
+
+  slide <<-EOS, :code
+    X = 1
+
+    module M
+      X # => 1
+    end
+
+    M::X # => NameError
+  EOS
+
+  slide <<-EOS, :center
+    Trade-off
+  EOS
+
+  slide <<-EOS, :code
+    # active_support/dependencies.rb
+
+    from_mod.parents.any? do |p|
+      p.const_defined?(const_name, false)
+    end
+  EOS
+
+  slide <<-EOS, :center
+    No attempt is made to follow ancestors
+  EOS
+
+  slide <<-EOS, :center
+    Corollary: Active Support does not emulate
+    constant name resolution algorithms
+  EOS
+end
+
+section "Request Flow" do
+  slide <<-EOS, :code
+    config.cache_classes
+  EOS
+
+  slide <<-EOS, :code
+    ActiveSupport::FileUpdateChecker
+  EOS
+
+  slide <<-EOS, :code
+    config.autoload_once_paths
+    config.explicitly_unloadable_constants
+  EOS
+
+  slide <<-EOS, :code
+    # rails/application.rb
+
+    unless config.cache_classes
+      middleware.use ActionDispatch::Reloader, ...
+    end
+  EOS
+
+  slide <<-EOS, :block
+    What is watched and reloaded:
+
+      * Routes
+
+      * Locales
+
+      * Application files:
+          
+          - Ruby files under autoload_*
+
+          - db/(schema.rb|structure.sql)
+  EOS
+
+  slide <<-EOS, :center
+    If files have changed, autoloaded constants
+    are wiped at the beginning of the request
+  EOS
+
+  slide <<-EOS, :code
+    # active_support/dependencies.rb
+
+    autoloaded_constants.each do |const|
+      remove_constant const
+    end
+
+    explicitly_unloadable_constants.each do |const|
+      remove_constant const
+    end
+  EOS
+
+  slide <<-EOS, :center
+    Constant access triggers const_missing again
+    because the constants are gone
+  EOS
+end
+
+section "That's all, thanks!" do
+end
+
+__END__
+
+section 'Constant Autoloading Gotchas' do
+end
+
+slide <<-EOS, :block
+  Problem 3: Ruby autoload
+EOS
+
+slide <<-EOS, :center
+  autoload uses require
+EOS
+
+slide <<-EOS, :block
+  require 'foo'
+
+  # TROLOLOL
+  $".delete_at(-1)
+
+  require 'foo'
+EOS
+
+slide <<-EOS, :center
+  # app/models/admin.rb
+  # app/models/admin/user.rb
+
+  module Admin
+    autoload :User, 'app/models/admin/user'
+  end
+EOS
+
+slide <<-EOS, :block
+  no API for removing autoloads
+EOS
