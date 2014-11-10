f2a1f066b2f596bfe022b586b8ba10429df38b47
NWHirschfeld <Niclas@NWHirschfeld.de>
2014-11-10 23:04:13 +0100

    underlined bold headlines as an argument

--- examples/sample.rb
+++ examples/sample.rb
@@ -1,17 +1,42 @@
 # encoding: utf-8
 
+
+
 center <<-EOS
-	center
-	some text
+center
+some text
 EOS
 
+
+
+
 tableofcontents
+
 tableofcontents "Custom Headline"
+
+
+
+
 
 block <<-EOS
 	block
 	some text
 EOS
+
+
+
+
+
+center <<-EOS, <<-HL
+slide-content
+...
+EOS
+My custom Headline Slide
+HL
+
+
+
+
 
 center <<-EOS
 	#{"dirty color hack".green}
@@ -21,6 +46,13 @@ center <<-EOS
 	not just normal text
 	#{"see colored 1.2 doku for more info".green_on_black}
 EOS
+
+
+
+
+
+
+
 
 section "my first section" do
 	center "first slide"
